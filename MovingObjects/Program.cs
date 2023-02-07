0c062a573c0ce9c81cb461e0152a6816339d6187
GET Academy <terje@getacademy.no>
2023-02-07 09:12:27 +0100

    Initial commit

---
+++ MovingObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using MovingObjects;
+
+//var obj = new BouncingBall(15, 3, 1, 1);
+var obj = new TeleportingPlus(15, 3, 1, 1);
+Console.CursorVisible = false;
+while (true)
+{
+    Console.Clear();
+    obj.Show();
+    obj.Move();
+    Thread.Sleep(50);
+}
---
+++ MovingObjects/TeleportingPlus.cs
@@ -0,0 +1,31 @@
+﻿namespace MovingObjects
+{
+    internal class TeleportingPlus
+    {
+        private int _col;
+        private int _row;
+        private int _speedCol;
+        private int _speedRow;
+
+        public TeleportingPlus(int col, int row, int speedCol, int speedRow)
+        {
+            _col = col;
+            _row = row;
+            _speedCol = speedCol;
+            _speedRow = speedRow;
+        }
+
+        public void Show()
+        {
+            Console.CursorLeft = _col;
+            Console.CursorTop = _row;
+            Console.Write("+");
+        }
+
+        public void Move()
+        {
+            _col = (_col + _speedCol) % Console.WindowWidth;
+            _row = (_row + _speedRow) % Console.WindowHeight;
+        }
+    }
+}
